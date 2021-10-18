5754841f9707e3f01456114ba532262fff6b1637
Aigul <aigul.sibgatulli@mail.ru>
2021-10-18 12:46:31 +0300

    add popup window

--- src/main.js
+++ src/main.js
@@ -4,7 +4,24 @@ require("dotenv").config();
 
 var viz;
 
+function verify() {
+  const textPlayer = document.getElementById("textPlayer").value;
+  const textRecords = document.getElementById("textRecords").value;
+  console.log(textPlayer, textRecords);
+  if (
+    (textPlayer === null || textPlayer === "") &&
+    (textRecords === null || textRecords === "")
+  ) {
+    console.log("both fields are empty");
+    return false;
+  } else return true;
+}
+
 function draw() {
+  console.log("draw");
+  if (!verify()) {
+    return;
+  }
   var config = {
     container_id: "viz",
     server_url: process.env.PATH,
@@ -28,6 +45,40 @@ function draw() {
   viz.render();
 }
 
+function handleChange() {
+  const radioPlayer = document.getElementById("radioPlayer");
+  const radioRecords = document.getElementById("radioRecords");
+  const radioFull = document.getElementById("radioFull");
+  var textPlayer = document.getElementById("textPlayer");
+  var textRecords = document.getElementById("textRecords");
+
+  if (radioPlayer.checked) {
+    textPlayer.disabled = false;
+    textRecords.disabled = true;
+    textRecords.value = "";
+  } else if (radioRecords.checked) {
+    textRecords.disabled = false;
+    textPlayer.disabled = true;
+    textPlayer.value = "";
+  } else if (radioFull.checked) {
+    textPlayer.disabled = true;
+    textRecords.disabled = true;
+    textPlayer.value = "";
+    textRecords.value = "";
+  }
+}
+
 window.onload = function () {
-  draw();
+  document
+    .getElementById("radioPlayer")
+    .addEventListener("change", handleChange);
+  document
+    .getElementById("radioRecords")
+    .addEventListener("change", handleChange);
+  document.getElementById("radioFull").addEventListener("change", handleChange);
+  const button = document.querySelector(".trigger");
+  button.onclick = function (e) {
+    e.preventDefault();
+    draw();
+  };
 };

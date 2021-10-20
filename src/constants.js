c79a12ace9d3b109e733fa494e6e55010056fa3b
Aigul <aigul.sibgatulli@mail.ru>
2021-10-20 15:12:38 +0300

    fix encrypted

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,3 @@
-// export const DB_PATH = "bolt://localhost:7687",
-//   DB_USER = "neo4j",
-//   DB_PASSWORD = "hanabi";
-
 export const DB_PATH = "neo4j://abdf20aa.databases.neo4j.io",
   DB_USER = "neo4j",
   DB_PASSWORD = "B4uwAXSsSNmGXJjBlUVT5ejkJN3ym2HMxeZmNaTj3Co";

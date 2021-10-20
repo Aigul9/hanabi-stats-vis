7acdaaea2bf07a8ea8f6c3caf5baffdd6f6867ce
Aigul <aigul.sibgatulli@mail.ru>
2021-10-20 20:47:11 +0300

    Updates

--- main.1f19ae8e.js
+++ main.1f19ae8e.js
@@ -225,7 +225,7 @@ function createQuery() {
       var limitPerPlayer = Math.ceil(textRecords / list.length); // limit clause
 
       var limit_q = "LIMIT ".concat(limitPerPlayer, " UNION ALL ");
-      if (textWeight !== "") where_q = "and ".concat(where_q);
+      if (textWeight !== "") where_q = "and ".concat(where_q, " ");
       list = list.map(function (player) {
         return "".concat(match_q, " WHERE p.name = '").concat(player, "' ").concat(where_q).concat(return_q, " ").concat(limit_q);
       });
